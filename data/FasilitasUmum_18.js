2761668375d4c2db2af3fc8b91fe9949ce17f3f9
alifiqbalrizki <alif.iqbal.rizki@mail.ugm.ac.id>
2023-09-26 18:25:01 +0700

    commit file

---
+++ data/FasilitasUmum_18.js
@@ -0,0 +1,24 @@
+var json_FasilitasUmum_18 = {
+"type": "FeatureCollection",
+"name": "FasilitasUmum_18",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "Jenis": "Kesehatan", "Keterangan": "Praktek Dr. Ahmad Riyanto" }, "geometry": { "type": "Point", "coordinates": [ 110.404564230855925, -7.88189430132266 ] } },
+{ "type": "Feature", "properties": { "Jenis": "Kesehatan", "Keterangan": "Puskesmas Pembantu Segoroyoso" }, "geometry": { "type": "Point", "coordinates": [ 110.408673208797239, -7.877135260915794 ] } },
+{ "type": "Feature", "properties": { "Jenis": "Kesehatan", "Keterangan": "Puskeswan Pleret" }, "geometry": { "type": "Point", "coordinates": [ 110.41442727343437, -7.883053498983791 ] } },
+{ "type": "Feature", "properties": { "Jenis": "Pemerintahan", "Keterangan": "Kantor Kepala Desa Segoroyoso" }, "geometry": { "type": "Point", "coordinates": [ 110.412575301881262, -7.880550884309747 ] } },
+{ "type": "Feature", "properties": { "Jenis": "Pendidikan", "Keterangan": "SMP N 3 Pleret" }, "geometry": { "type": "Point", "coordinates": [ 110.41412441395137, -7.883191439732557 ] } },
+{ "type": "Feature", "properties": { "Jenis": "Pendidikan", "Keterangan": "SD N Segoroyoso" }, "geometry": { "type": "Point", "coordinates": [ 110.412688730295912, -7.879908117411547 ] } },
+{ "type": "Feature", "properties": { "Jenis": "Pendidikan", "Keterangan": "TK ABA Dahromo" }, "geometry": null },
+{ "type": "Feature", "properties": { "Jenis": "Pendidikan", "Keterangan": "SD Muhammadiyah Wonokromo 2" }, "geometry": null },
+{ "type": "Feature", "properties": { "Jenis": "Pendidikan", "Keterangan": "SD Karanggayam" }, "geometry": { "type": "Point", "coordinates": [ 110.3979859968738, -7.879602668677762 ] } },
+{ "type": "Feature", "properties": { "Jenis": "Ibadah", "Keterangan": "Musholla Al-Mukhayat" }, "geometry": { "type": "Point", "coordinates": [ 110.399697662447778, -7.881370832779102 ] } },
+{ "type": "Feature", "properties": { "Jenis": "Ibadah", "Keterangan": "Musholla Nurul Huda Dahromo" }, "geometry": { "type": "Point", "coordinates": [ 110.403464331763786, -7.882909836859096 ] } },
+{ "type": "Feature", "properties": { "Jenis": "Ibadah", "Keterangan": "Musholla As-Shidiq" }, "geometry": { "type": "Point", "coordinates": [ 110.399561562613286, -7.882378762965633 ] } },
+{ "type": "Feature", "properties": { "Jenis": "Ibadah", "Keterangan": "Musholla Nur Hisyam" }, "geometry": { "type": "Point", "coordinates": [ 110.401456399004516, -7.880489254860249 ] } },
+{ "type": "Feature", "properties": { "Jenis": "Ibadah", "Keterangan": "Musholla Tanihul Ghofilin" }, "geometry": { "type": "Point", "coordinates": [ 110.414307630754763, -7.879594413918183 ] } },
+{ "type": "Feature", "properties": { "Jenis": "Ibadah", "Keterangan": "Masjid An-Nur" }, "geometry": { "type": "Point", "coordinates": [ 110.395564043149875, -7.878258279691423 ] } },
+{ "type": "Feature", "properties": { "Jenis": "Ibadah", "Keterangan": "Masjid Al-Huda" }, "geometry": { "type": "Point", "coordinates": [ 110.413264496220236, -7.880894351841668 ] } },
+{ "type": "Feature", "properties": { "Jenis": "Ibadah", "Keterangan": "Masjid Roudhoh" }, "geometry": { "type": "Point", "coordinates": [ 110.399143446914707, -7.879768350836012 ] } }
+]
+}
